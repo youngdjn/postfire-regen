e6f663a6688320e1dc301b2a0a9badd72f3497fc
Derek Young <derek.j.n.young@gmail.com>
2016-12-02 15:50:33 -0800

    Added regen analysis placeholder file

---
+++ regen_analysis.R
@@ -0,0 +1,7 @@
+setwd("~/UC Davis/Research Projects/Post-fire regen/Dev/postfire-regen")
+
+# open intermediate data files
+
+d.plot <- read.csv("data_intermediate/plot_level.csv",header=T,stringsAsFactors=FALSE)
+d.sp <- read.csv("data_intermediate/speciesXplot_level.csv",header=T,stringsAsFactors=FALSE)
+
