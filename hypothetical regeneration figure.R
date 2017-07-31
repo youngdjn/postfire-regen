1385227def7df654adb659f6d39889f96d7b008d
Derek Young <derek.j.n.young@gmail.com>
2017-07-30 22:07:44 -0700

    Hypothetical regeneration figure (Fig. 1)

---
+++ hypothetical regeneration figure.R
@@ -0,0 +1,28 @@
+setwd("C:/Users/DYoung/Documents/UC Davis/Research Projects/Post-fire regen/Dev/postfire-regen")
+
+s <- seq(-2.2,2.2,by=.1)
+
+
+dry <- dnorm(s,mean=1)
+wet <- dnorm(s,mean=-1)
+
+d.dry <- data.frame(s,prob=dry,norm="Dry")
+d.wet <- data.frame(s,prob=wet,norm="Wet")
+
+d <- rbind(d.dry,d.wet)
+
+p <-ggplot(d,aes(x=s,y=prob,color=norm)) +
+  geom_line(size=1.5) +
+  scale_color_manual(values=c("Wet"="turquoise4","Dry"="darkorange1")) +
+  theme_bw(16)+
+  labs(x="Post-fire precipitation anomaly",y="Seedling recruitment",color="Normal climate") +
+  geom_vline(xintercept=0,linetype="dashed") +
+  theme(axis.text.x = element_text(size=14),axis.text.y = element_text(size=14)) +
+  scale_x_continuous(breaks=c(-2.2,0,2.2), labels= c("unusually dry","0","unusually wet")) +
+  scale_y_continuous(breaks=c(0,0.4),labels=c("0","high")) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(legend.position = c(0.8, 0.4),plot.margin = unit(c(.5,2,.5,.5), "cm"))
+
+tiff(file=paste0("../Figures/Fig1_hypothetical_recruitment_",Sys.Date(),".tiff"),width=1500,height=1000,res=200) 
+p
+dev.off()
